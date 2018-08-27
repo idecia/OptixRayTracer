f93e7108d3c9185d4d1892f212e77dc9da650465
Ignacio <idecia@fing.edu.uy>
2018-08-27 16:39:39 -0300

    ok

--- OptixRayTracer/geometry/Mesh3D.h
+++ OptixRayTracer/geometry/Mesh3D.h
@@ -2,6 +2,8 @@
 
 #include "core/optix_global.h"
 #include "geometry/Face.h"
+#include "geometry/Polygon3D.h"
+#include "geometry/Polygon2D.h"
 #include <vector>
 
 using namespace std;
@@ -12,6 +14,9 @@ private:
 
 	vector<float3> vertices;
 	vector<Face*> faces;
+
+	Polygon3D* FaceToPoly3D(const Face* &f);
+	Polygon2D* ProjectFace(const Face* &f);
 
 public:
 
@@ -54,30 +59,146 @@ Mesh3D::~Mesh3D() {
 	}
 }
 
+
+void Append(vector<Face*> &vec1, vector<Face*> &vec2) {
+
+	int n = vec2.size();
+	for (int i = 0; i < n; i++) {
+		vec1.push_back(vec2[i]);
+	}
+}
+
+float ValueAt(float3 v, int i) {
+	if (i == 0)
+		return v.x;
+	if (i == 1)
+		return v.y;
+	return v.z;
+}
+
+void GetProjectionAxes(const float3 &normal, int &u, int &v) {
+
+	u = 0; v = 1;
+	if (abs(normal.y) > abs(normal.x) &&
+		abs(normal.y) > abs(normal.z)) {
+		u = 0;
+		v = 2;
+	}
+	else if (abs(normal.x) > abs(normal.y) &&
+		abs(normal.x) > abs(normal.z)) {
+		u = 1;
+		v = 2;
+	}
+
+}
+
+float2 ProjectVec3DTo2D(float3 vec, int u, int v) {
+
+	return make_float2(ValueAt(vec, u), ValueAt(vec, v));
+
+}
+
+Polygon2D* ProjectPoly3D(Polygon3D* poly3D) {
+
+	float3 normal = poly3D->NewellNormal();
+	int u, v;
+	GetProjectionAxes(normal, u, v);
+	Polygon2D* poly2D = new Polygon2D();
+	const vector<float3> &vertices = poly3D->GetVertices();
+	for (int i = 0; i < vertices.size(); i++) {
+		float3 vec = vertices[i];
+		poly2D->AddVertex(ProjectVec3DTo2D(vec, u, v);
+	}
+	return poly2D;
+}
+
+Polygon3D* Mesh3D::FaceToPoly3D(const Face* &f) {
+
+	Polygon3D* poly3D = new Polygon3D();
+	const vector<int> &ind = f->GetIndices();
+	int n = ind.size();
+	for (int i = 0; i < n; i++) {
+		poly3D->AddVertex(vertices[ind[i]]);
+	}
+	return poly3D;
+}
+
+Polygon2D* Mesh3D::ProjectFace(const Face* &f) {
+
+	Polygon3D* poly3D = FaceToPoly3D(f);
+	Polygon2D* poly2D = ProjectPoly3D(poly3D);
+	delete poly3D;
+	return poly2D;
+
+}
+
+vector<Face*> TriangulatePoly2D(Polygon2D* poly2D) {
+
+	vector<Face*> faces;
+	const vector<float2> &vertices = poly2D->GetVertices();
+	int n = vertices.size();
+	vector<int> prev(n), next(n);
+	for (int i = 0; i < n; i++) {
+		prev[i] = i - 1;
+		next[i] = i + 1;
+	}
+	prev[0] = n - 1;
+	next[n - 1] = 0;
+
+	WindingType PW = poly2D->PolygonWinding();
+	int i = 0;
+	while (n > 3) {
+
+		bool isEar = 1;
+		const float2 &a = vertices[prev[i]];
+		const float2 &b = vertices[i];
+		const float2 &c = vertices[next[i]];
+		if (TriangleWinding(a, b, c) == PW) {
+			int k = next[next[i]];
+			do {
+				if (TestPointTriangle(a, b, c, vertices[k])) {
+					isEar = 0;
+					break;
+				}
+				k = next[k];
+			} while (k != prev[i]);
+		}
+		else {
+			isEar = 0;
+		}
+
+		if (isEar) {
+			Face* f = new Face(ind[prev[i]], ind[i], ind[next[i]]);
+			faces.push_back(f);
+			next[prev[i]] = next[i];
+			prev[next[i]] = prev[i];
+			i = prev[i];
+			n--;
+		}
+		else {
+			i = next[i];
+		}
+
+	}
+	if (n == 3) {
+		Face* f = new Face(ind[prev[i]], ind[i], ind[next[i]]);
+		faces.push_back(f);
+	}
+	return faces;
+
+}
+
 void Mesh3D::Triangulate() {
 
-	/*for (int i = 0; i < faces.size(); i++) {
-		
-		Face* oldFace = faces[i];
-		vector<int> ind = oldFace->GetIndices();
-		Polygon3D* poly = new Polygon2D();
-		for (int j = 0; j < ind.size(); j++){
-			poly.AddVertex(vertices[ind[j]]);
-		}
-		float3 normal = poly.NewellNormal();
-
-
-
-
-		Face* newFace = new Face();*/
-
-
-
-
-
-
-	
-	
-
+	vector<Face*> triFaces;
+	int nFaces = faces.size()
+	for (int i = 0; i < nFaces; i++) {
+		Polygon2D* poly2D = ProjectFace(faces[i]);
+		vector<Face*> newFaces = TriangulatePoly2D(poly2D);
+		delete poly2D;
+		Append(triFaces, newFaces)
+	}
+	faces.clear();
+	faces = triFaces;
 
 }
