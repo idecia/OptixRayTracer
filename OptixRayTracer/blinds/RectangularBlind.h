31183691cfe9b9cb31d7391e9a806b3c44d2a67e
unknown <idecia@fing.edu.uy>
2019-09-26 01:45:49 -0300

    things

--- OptixRayTracer/blinds/RectangularBlind.h
+++ OptixRayTracer/blinds/RectangularBlind.h
@@ -19,6 +19,7 @@ public:
 
 	RectangularBlind(float w, float t, float l, float a, float s, int N, float H, float W, float L);
 	RectangularBlind(float w, float t, float l, float a, float s, int N, float c, float H, float W, float L);
+	void RepairBlind();
 	 Mesh3D* GetMesh();
 	 float w;
 	 float t;
@@ -32,8 +33,40 @@ public:
 	 float W;
 	 float L;
 
+	 float Rext;
+
+	 static float wmin; 
+	 static float wmax;
+	 static float tmin ;
+	 static float tmax;
+	 static float lmin;
+	 static float lmax ;
+	 static float amin ;
+	 static float amax;
+	 static float smin ;
+	 static float smax ;
+	 static float cmin ;
+	 static float cmax;
+	 static int NMax;
+
+
 
 };
+
+
+ float RectangularBlind::wmin = 0.1f;
+  float RectangularBlind::wmax = 0.25f;
+  float RectangularBlind::tmin = 0.005;
+  float RectangularBlind::tmax = 0.01;
+  float RectangularBlind::lmin = 1.0f;
+  float RectangularBlind::lmax = 2.0f;
+  float RectangularBlind::amin = 0.0f;
+  float RectangularBlind::amax = M_PIf / 2.0f;
+  float RectangularBlind::smin = 0.0f;
+  float RectangularBlind::smax = 1.0f;
+  float RectangularBlind::cmin = 0.0f;
+  float RectangularBlind::cmax = wmax / 2.0f;
+  int RectangularBlind::NMax = 10;
 
 
 void GetWorldMesh(GeometricObject* obj, Transformation objToWorld, int &vertexOffset, Mesh3D* mesh) {
@@ -79,13 +112,13 @@ Mesh3D* GetWorldMesh(GeometricObject* obj) {
 
 }
 
-RectangularBlind::RectangularBlind(float w, float t, float l, float a, float s, int N, float H, float W, float L)
-	: w(w), t(t), l(l), a(a), s(s), N(N), H(H), W(W), L(L)  {
-
-}
 
 RectangularBlind::RectangularBlind(float w, float t, float l, float a, float s, int N, float c, float H, float W, float L)
 	: w(w), t(t), l(l), a(a), s(s), N(N), c(c), H(H), W(W), L(L) {
+
+	if (c > 0) 
+		Rext = ((4 * c*c) + (w*w)) / (8 * c);
+
 
 }
 
@@ -132,13 +165,73 @@ RectangularBlind::RectangularBlind(float w, float t, float l, float a, float s, 
 } 
 */
 
+void RectangularBlind::RepairBlind() {
+
+	w = min(w, wmax);
+	w = max(w, wmin);
+
+	l = min(l, lmax);
+	l = max(l, lmin);
+
+	a = min(a, amax);
+	a = max(a, amin);
+
+	s = min(s, smax);
+	s = max(s, smin);
+
+	c = min(c, cmax);
+	c = max(c, cmin);
+
+	if (2 * c > w)
+		c = w / 2.0f;
+
+	if ((c > 0) && s < w) {
+		float aux = acos(s / (2.0f*Rext)) - acos(w / (2.0f*Rext));
+		if (a < aux)
+			a = aux;
+	}
+
+	float h = 1.0f;
+	float aux;
+	if ((c > 0) && (sinf(a) < w / (2 * Rext)))
+		aux = L / (Rext*(1 - cosf(a)) + c*cosf(a) + (w / 2.0f)*sinf(a));
+	else
+		aux = L / (w*sinf(a));
+
+	if (aux < 1.0f)
+		h = aux;
+
+	w = h*w;
+	c = h*c;
+	Rext = h*Rext;
+
+	h = 1.0f;
+	if ((c > 0) && (cosf(a) < w / (2 * Rext)))
+		aux = H / (Rext*(1 - sinf(a)) + c*sinf(a) + (w / 2.0f)*cosf(a));
+	else
+		aux = H / (w*cosf(a));
+
+	if (aux < 1.0f)
+		h = aux;
+
+	w = h*w;
+	c = h*c;
+	Rext = h*Rext;
+
+	N =  (int)floorf((H - (H/aux))/s + 1);
+	N = min(N, NMax);
+
+}
+
 Mesh3D*  RectangularBlind::GetMesh() {
 
 	Polygon2D poly;
-	float Rext;
 	float wOver2 = w / 2.0f;
+	float sinAlpha = sinf(a);
+	float cosAlpha = cosf(a);
+	float LBlind = w*sinAlpha;
+	float HBlind = w*cosAlpha;
 	if (c > 0) {
-		 Rext  = ((4*c*c) + (w*w)) / (8*c);
 		float thetaMin = acos(w / (2 * Rext));
 		float thetaMax = M_PIf - thetaMin;
 		float nArcs = 5;
@@ -155,6 +248,12 @@ Mesh3D*  RectangularBlind::GetMesh() {
 			float x = Rint*cos(theta);
 			float y = Rint*sin(theta);
 			poly.AddVertex(make_float2(x, y));
+		}
+		if (sinAlpha < w/(2 * Rext)) {
+			LBlind = Rext*(1 - cosAlpha) + c*cosAlpha + wOver2*sinAlpha;
+		}
+		if (cosAlpha < w / (2 * Rext)) {
+			HBlind = Rext*(1 - sinAlpha) + c*sinAlpha + wOver2*cosAlpha;
 		}
 	}
 	else {
@@ -176,9 +275,12 @@ Mesh3D*  RectangularBlind::GetMesh() {
 	//slat->Triangulate();
 	slat->RotateY(-M_PIf / 2.0f);
 	slat->RotateX(a);
-	float infDist = 2 * c*sinf(a) - w*cosf(a);
-	float pad = (H - infDist - s*(N - 1)) / 2.0f;
-	slat->Translate(make_float3(0.0f, -L / 2.0f, pad + infDist / 2.0f));
+	
+	float shiftH = c*sinAlpha + wOver2*cosAlpha;
+	float shiftL = -L + c*cosAlpha + wOver2*sinAlpha;
+	float padH = (H - HBlind - s*(N - 1)) / 2.0f;
+	float padL = (L - LBlind) / 2.0f;
+	slat->Translate(make_float3(0.0f, shiftL +padL, padH + shiftH));
 
 	GeometricObject* blind = new Compound();
 	vector<Instance*> instances;
--- OptixRayTracer/core/main.cpp
+++ OptixRayTracer/core/main.cpp
@@ -463,7 +463,8 @@ int main(int argc, char* argv[]) {
 //	RectangularBlind Blind(w, h, l, a, p, n); //ver esto
 	//RectangularBlind(float w , float h, float l, float a, float S, int n, float c, float H, float W, float L)
 	//RectangularBlind Blind(0.15, 0.005  , 2.0    , 0.5    , 0.2   , 10   , 0.02    ,   1.5  , 2      , 0.25); 
-	RectangularBlind Blind(0.15, 0.01, 2.0, 1.57, 0.2, 7, 0.07, 1.5, 2, 0.25);
+	RectangularBlind Blind(0.15, 0.001, 2.0, 0.0, 0.0001, 4, 0.03, 1.5, 2, 0.25);
+	Blind.RepairBlind();
 	ExportModel("blind.obj", Blind.GetMesh());
 	//Mesh3D* mesh = Blind.GetMesh(); 
 	//optimize(argc, argv);
